e5be287a624378ce794dd121332080eda4583124
satsuki-yabu <tetecoo.1019@gmail.com>
2020-11-01 17:59:10 +0900

    scss適用

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from './layout'
+import  './scss/header.scss'
+
+const Header = () => {
+  return (
+    <>
+      <div className='top_link'>
+        <h2>TOP</h2>
+      </div>
+      {/* <div className='other_link'>
+        <ul>
+          <li></li>
+        </ul>
+      </div> */}
+    </>
+  )
+}
+
+export default Header
