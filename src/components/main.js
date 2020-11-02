071d04832dd6d40e23b4920716e99e95fdd47723
satsuki-yabu <tetecoo.1019@gmail.com>
2020-11-02 13:32:10 +0900

    タスク追加機能

--- src/components/main.js
+++ src/components/main.js
@@ -1,20 +1,41 @@
-import React from 'react'
+
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
 import css from './scss/main.scss'
 
 
 const Main = () => {
+
+  // 入力結果を受け取る
+  const [todo, setTodo] = useState('')
+
+  const [incompleteTodo, setIncompleteTodo] = useState(['ううう','eee'])
+
+  const onChangeAddTodo = (event) => setTodo(event.target.value);
+
+  const addTodo = () => {
+    const newTodoList = [...incompleteTodo, todo];
+    setIncompleteTodo(newTodoList);
+  }
+  
   return (
     <>
       <div className={css.todo}>
         <div className={css.input_todo}>
           <h3>TODO入力</h3>
-          <input type=''></input>
-          <button>入力</button>
+          <input type='text' value={todo} onChange={onChangeAddTodo}></input>
+          <button onClick={addTodo}>入力</button>
         </div>
         <div className={css.incomplete_todo}>
           <h3>やることリスト</h3>
             <ul>
-              <li>あああ</li>
+             {incompleteTodo.map((todo)=>{
+               return (
+                 <div key={todo} className='incomplete_todo_list'>
+                   <li>{todo}</li>
+                 </div>
+               )
+             })}
             </ul>
         </div>
         <div className={css.complete_todo}>
