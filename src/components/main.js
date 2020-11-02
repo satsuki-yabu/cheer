164b337cf7c2e1d8530c19bb93c66ea570d78929
satsuki-yabu <tetecoo.1019@gmail.com>
2020-11-02 10:16:01 +0900

    形ざっくり

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import css from './scss/main.scss'
 
+
 const Main = () => {
   return (
     <>
-      <div className={css.title}>今日１日をふりかえってみましょう</div>
-        <div className={css.daywork}>
-          <button>頑張った！</button>
-          <button>そこそこ頑張ったかな〜</button>
-          <button>うう、頑張ってないかも...</button>
-          <button>だめだ〜！なんもできてない！</button>
+      <div className={css.todo}>
+        <div className={css.input_todo}>
+          <h3>TODO入力</h3>
+          <input type=''></input>
+          <button>入力</button>
         </div>
+        <div className={css.incomplete_todo}>
+          <h3>やることリスト</h3>
+            <ul>
+              <li>あああ</li>
+            </ul>
+        </div>
+        <div className={css.complete_todo}>
+          <h3>完了リスト</h3>
+            <ul>
+              <li>いいい</li>
+            </ul>
+        </div>
+      </div>
     </>
   )
 }
