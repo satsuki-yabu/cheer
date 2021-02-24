c6c321cc0189c433047e9e010ae3ec73e2de8492
satsuki-yabu <tetecoo.1019@gmail.com>
2021-02-24 13:39:01 +0900

    テスト用にmasterブランチを変更した

--- src/pages/top.js
+++ src/pages/top.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import Link from 'next/link'
-import Layout from '../components/layout'
-import Header from '../components/header'
-import css from '../components/scss/top.scss'
+import React from 'react';
+import Link from 'next/link';
+import Layout from '../components/layout';
+import Header from '../components/header';
+import css from '../components/scss/top.scss';
 
 const Top = () => {
-  return (
-    <>
-    <Header />
-    <div className={css.goTodo}>
-      <h1>Todo画面にいく</h1>
-    </div>
-    <div className={css.goCheer}>
-      <h1>応援画面にいく</h1>
-    </div>
-    </>
-  )
-}
+	return (
+		<>
+			<Header />
+			<div className={css.goTodo}>
+				<h1>テストします</h1>
+			</div>
+			<div className={css.goCheer}>
+				<h1>テストします</h1>
+			</div>
+		</>
+	);
+};
 
-export default Top
+export default Top;
